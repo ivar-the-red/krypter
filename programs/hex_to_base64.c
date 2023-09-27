c06d54bc89a1add8c7576c321e3441b98890f262
ivar_the_red <ivar_the_red@protonmail.com>
2023-09-27 15:06:36 +0100

    Tidy up
    Remove comments and tidy up code to ease debugging.
    
    Signed-off-by: ivar_the_red <ivar_the_red@protonmail.com>

--- programs/hex_to_base64.c
+++ programs/hex_to_base64.c
@@ -8,50 +8,21 @@
 
 int main(int argc, char **argv)
 {
-    // const char *input_hex = "deadbeef";
+    // const char *in_hex = "deadbeef";
+    // char *out_b64;
 
-    // printf("Bytes allocated to array for \"%s\" = %d\n", "deadbeef", hex_to_b64("deadbeef"));
+    // int result_test = hex_to_b64(in_hex, &out_b64);
 
-    // unsigned char byte_arr[4];
-    // size_t byte_arr_size = sizeof(byte_arr) / sizeof(byte_arr[0]);
+    // printf("Converting \"%s\" to base64 yeilds \"%s\".\n Output code was %d\n", in_hex, out_b64, result_test);
 
-    // int num_bytes = 0;
-    // num_bytes = hex_to_bytes(input_hex, byte_arr, byte_arr_size);
-
-    // printf("There were %d bytes written.\n", num_bytes);
-
-    // printf("Bytes needed for %d is %zu.\n", 8, get_b64_len(8));
-    // printf("Bytes needed for %d is %zu.\n", 16, get_b64_len(16));
-
-    // char *b64_result = bytes_to_b64(byte_arr, byte_arr_size);
-
-    // printf("Converting \"%s\" to base64 yeilds \"%s\".\n", input_hex, b64_result);
-
-    // //Cryptopals data
-    // const char *cp_hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    // unsigned char cp_byte_array[48];
-    // size_t cp_byte_arr_size = sizeof(cp_byte_array) / sizeof(cp_byte_array[0]);
-    // printf("cp_byte_array_size =  %zu\n", cp_byte_arr_size);
-    // int bytes_written = 0;
-
-    // bytes_written = hex_to_bytes(cp_hex, cp_byte_array, cp_byte_arr_size);
-    // printf("Translating from hex wrote %d bytes.\n", bytes_written);
-    // char *cp_b64_result = bytes_to_b64(cp_byte_array, cp_byte_arr_size);
-
-    // printf("Converting \"%s\" to base64 yeilds \"%s\".\n", cp_hex, cp_b64_result); //WRONG RESULT - DEBUG
-
-    const char *in_hex = "deadbeef";
-    char *out_b64, *cp_b64;
-
-    int result = hex_to_b64(in_hex, &out_b64);
-
-    printf("Converting \"%s\" to base64 yeilds \"%s\".\n Output code was %d\n", in_hex, out_b64, result);
+    // free(out_b64);
 
     const char *cp_hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    char *cp_b64;
 
-    result = hex_to_b64(cp_hex, &cp_b64);
+    int result = hex_to_b64(cp_hex, &cp_b64);
     printf("Converting \"%s\" to base64 yeilds \"%s\".\n Output code was %d\n", cp_hex, cp_b64, result);
     
-    free(out_b64);
+    free(cp_b64);
     return EXIT_SUCCESS;
 }
